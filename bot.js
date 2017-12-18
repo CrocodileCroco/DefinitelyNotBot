bfed7bf1f357e8668f6178f152a741953c7762d6
Smrman <vinoa.89.jeux@gmail.com>
2017-12-18 17:12:46 +0100

    Update bot.js
    Code un peu nettoyé

--- bot.js
+++ bot.js
@@ -1,63 +1,48 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var date = new Date();
-var current_hour = date.getHours();
+let date = new Date();
+let current_hour;
+let reponse;
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
+// juste envoyer
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.channel.send('PONG!');
   	}
 });
 
+//répondre à la personne
 client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'rageux') {
-    	message.reply('je suis literallement en trains de rire sur ta face');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'croco') {
-    	message.reply('oui? si c pour me provoquer je te dit retourne dans ton coin caillou , si c l inverse dit toi quil a pas vraiment le temps de parler');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'troll') {
-    	message.reply('morsay et cortex cest du troll non professionel tout comme TOI');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'horloge') {
-    	message.reply('Il est ' + current_hour + ' heures aujourdhui');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'tg le bot') {
-    	message.reply('C pas gentil :(');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'nope') {
-    	message.reply('Correction : Nope.AVI');
-  	}
+	switch (message.content) {
+		case 'bing':
+			reponse = 'BONG!'
+			break;
+		case 'rageux':
+			reponse = 'je suis literallement en trains de rire sur ta face'
+			break;
+		case 'croco':
+			reponse = 'oui? si c pour me provoquer je te dit retourne dans ton coin caillou , si c l inverse dit toi quil a pas vraiment le temps de parler'
+			break;
+		case 'troll':
+			reponse = 'morsay et cortex cest du troll non professionel tout comme TOI'
+			break;
+		case 'horloge':
+			current_hour = date.getHours();
+			reponse = 'Il est ' + current_hour + ' heures aujourdhui'
+			break;
+		case 'tg le bot':
+			reponse = 'C pas gentil :('
+			break;
+		case 'nope':
+			reponse = 'Correction : Nope.AVI'
+			break;
+	}
+	message.reply(reponse);
 });
 
 
-
-
-
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
