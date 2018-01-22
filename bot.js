95ef3d974f413f3b32796f9483e32ef4a7f9df77
CrocodileCroco <CrocodileCroco@users.noreply.github.com>
2018-01-22 16:27:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var current_hour = date.getHours();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setUsername('AllezVouFairMettreHeroku')
+    client.user.setUsername('HerokuATuéCeBotRIP')
 });
 
 client.on('message', message => {
