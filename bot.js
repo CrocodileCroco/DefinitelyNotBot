792e5cb55fe336d76814dd476a8048df1b9c6066
CrocodileCroco <CrocodileCroco@users.noreply.github.com>
2018-04-01 18:10:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ var current_hour = date.getHours();
 client.on('ready', () => {
     console.log('I am ready!');
     client.user.setUsername('Kaede')
+    client.user.setActivity('manger du poisson', { type: 'blh' });
 });
 
 client.on('message', message => {
