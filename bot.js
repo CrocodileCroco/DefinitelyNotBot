dc036cf937a96279da1a0358ec1f54ba2439422c
CrocodileCroco <CrocodileCroco@users.noreply.github.com>
2018-05-06 13:48:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 var date = new Date();
 var current_hour = date.getHours();
 const prefix = "!";
+const command = args.shift().toLowerCase();
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -419,14 +420,15 @@ client.on('message', message => {
 });            
 
 client.on('message', message => {
-  if (message.content.startsWith(prefix + "say")) {
-      const args = message.content.slice(prefix.length).trim().split(/ +/g);  
-      const sayMessage = args.join(" ");
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+        if(command === "say") {
+        // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+        // To get the "message" itself we join the `args` back into a string with spaces: 
+        const sayMessage = args.join(" ");
         // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-        message.delete().catch(O_o=>{}); 
         // And we get the bot to say the thing: 
         message.channel.send(sayMessage);
-  }
+    }
 });            
 
 
