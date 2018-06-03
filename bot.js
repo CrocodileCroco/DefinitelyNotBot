6ae0f12f6e594c98cde7128669578dabfd595c57
CrocodileCroco <CrocodileCroco@users.noreply.github.com>
2018-06-03 18:25:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -579,7 +579,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '!stop') {
-    	client.leaveVoiceChannel(message.member.voiceState.channelID);
+    	voiceChannel.leave();
   	}
 });
 
